7574a10a37395ae81e3a76894cf084ee30cd5734
Hania Seif <145772855+hania-cs@users.noreply.github.com>
2024-04-13 00:15:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,62 @@
+function getComputersChoice(){
+    let options = ["rock", "paper", "scissor"]
+    let computersInput = Math.floor(Math.random()*3)
+    let compChoice = options[computersInput]
+    return compChoice
+}
+let userScore = 0
+let computerScore = 0
+let computerInput = getComputersChoice()
+
+function playRound(userInput, computerInput){
+    if(userInput === computerInput){
+        return "It's a tie!"
+    }
+    
+    else if(userInput === "rock"){
+        if(computerInput === "paper"){
+            computerScore++
+            return "You lose! Paper beats rock"
+        }
+        else if(computerInput === "scissor"){
+            userScore++
+            return "You win! rock beats scissor"
+        }
+    }
+    else if(userInput === "paper"){
+        if(computerInput === "rock"){
+            userScore++
+            return "You win! paper beats rock"
+        }
+        else if(computerInput==="scissor"){
+            computerScore++
+            return "You lose! scissor beats paper"
+        }
+    }
+    else if(userInput === "scissor"){
+        if(computerInput === "rock"){
+            computerScore++
+            return "You lose! rock beats scissors"
+        }
+        else if(computerInput === "paper"){
+            userScore++
+            return "You win! scissor beats paper"
+        }
+    }
+    
+}
+
+
+function playGame(){
+    for(let i = 0; i < 5; i++){
+        let userInp = prompt("Enter your choice: Rock/Paper/Scissor: ").toLowerCase()
+        console.log(playRound(userInp, computerInput))
+    }
+    if(userScore > computerScore){
+        return 'YOU WIN'
+    }
+    else{
+        return "YOU LOSE"
+    }
+}
+console.log(playGame())
